795cd2205a567ab4edd03e03a5f0e19574cc5c3e
souksili <ouksili.samy@yahoo.com>
2025-01-23 00:22:19 +0100

    Initial commit

--- données non-structurées/script.js
+++ données non-structurées/script.js
@@ -40,7 +40,6 @@ function processData(content) {
     const data = Object.keys(wordCount).map(word => ({ name: word, value: wordCount[word] }));
 
     createVisualization(data);
-    createBarChart(data);
 }
 
 function extractWords(text) {
@@ -54,7 +53,7 @@ function extractWords(text) {
 }
 
 function createVisualization(data) {
-    const width = 480;
+    const width = 960;
     const height = 600;
 
     const color = d3.scaleOrdinal(d3.schemeCategory10);
@@ -128,53 +127,4 @@ function createVisualization(data) {
         d.fx = null;
         d.fy = null;
     }
-}
-
-function createBarChart(data) {
-    const width = 480;
-    const height = 600;
-    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-
-    const svg = d3.select("#barChart")
-        .attr("viewBox", `0 0 ${width} ${height}`)
-        .style("font", "12px sans-serif");
-
-    const g = svg.append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
-
-    const x = d3.scaleBand()
-        .domain(data.map(d => d.name))
-        .range([0, innerWidth])
-        .padding(0.1);
-
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.value)])
-        .nice()
-        .range([innerHeight, 0]);
-
-    g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", `translate(0,${innerHeight})`)
-        .call(d3.axisBottom(x));
-
-    g.append("g")
-        .attr("class", "axis axis--y")
-        .call(d3.axisLeft(y).ticks(10, "s"))
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Fréquence");
-
-    g.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", d => x(d.name))
-        .attr("y", d => y(d.value))
-        .attr("width", x.bandwidth())
-        .attr("height", d => innerHeight - y(d.value));
 }
